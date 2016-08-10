f855ae8b8609d59fb6f563b4bb2436f558e5ae57
dejun <heng50202@126.com>
2016-08-10 15:53:27 +0800

    手势控制亮度和音量

--- vedioplayer/src/main/java/com/cui/zh/vedioplayer/full/CustomMediaController.java
+++ vedioplayer/src/main/java/com/cui/zh/vedioplayer/full/CustomMediaController.java
@@ -1,10 +1,14 @@
 package com.cui.zh.vedioplayer.full;
 
+import android.app.Activity;
 import android.content.Context;
+import android.media.AudioManager;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import com.cui.zh.vedioplayer.R;
@@ -31,8 +35,17 @@ public class CustomMediaController extends MediaController {
 
     private MediaPlayerControl mediaPlayerControl;
 
+    private final AudioManager audioManager;    // 调整音量的
+    private Window window;  // 调整亮度的
+    private final int maxVolume;    // 最大音量(获取到的)
+    private int currentVolume;  // 当前音量(在开始滑动手势时的音量)
+    private float currentBrightness;    // 当前亮度(在开始滑动手势时的亮度(0.0f - 1.0f,如果是负代表自动调整))
+
     public CustomMediaController(Context context) {
         super(context);
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        window = ((Activity)context).getWindow();
     }
 
     /** 重写这个方法(vitamio MediaController的)，来自定义MediaController的视图*/
@@ -72,31 +85,75 @@ public class CustomMediaController extends MediaController {
                 mediaPlayerControl.seekTo(position);
             }
         });
-        // 调整屏幕亮度(左边)和音量(右边)
-//        final View adjustView = view.findViewById(R.id.adjustView);
-//        final GestureDetector gesturedetector = new GestureDetector(getContext(),
-//                new GestureDetector.SimpleOnGestureListener(){
-//                    @Override
-//                    public boolean onScroll(MotionEvent e1, MotionEvent e2,
-//                                            float distanceX, float distanceY) {
-//                        float startX = e1.getX();
-//                        float startY = e1.getY();
-//                        float endX = e2.getX();
-//                        float endY = e2.getY();
-//
-//                        float width = adjustView.getWidth();
-//                        float height = adjustView.getHeight();
-//
-//
-//                        return true;
-//                    }
-//                });
-//        adjustView.setOnTouchListener(new OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
+        /**调整屏幕亮度(左边)和音量(右边)
+         * 1. 拿到View（整个视频播放区我们故意放了一个空的view）
+         * 2. 对View进行touch监听
+         * 3. 在touch监听里用手势处理
+         * 4. 完成屏幕左侧和右侧的判断
+         * 5. 完成我们的业务
+         * */
+        final View adjustView = view.findViewById(R.id.adjustView);
+        final GestureDetector gesturedetector = new GestureDetector(getContext(),
+                new GestureDetector.SimpleOnGestureListener(){
+                    @Override
+                    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                            float distanceX, float distanceY) {
+                        float startX = e1.getX();
+                        float startY = e1.getY();
+                        float endX = e2.getX();
+                        float endY = e2.getY();
+                        float width = adjustView.getWidth();
+                        float height = adjustView.getHeight();
+                        // 垂直移动距离占整个视图高度的比例(用来调整亮度和音量的)
+                        float percentage = (startY-endY) /height;
+                        // 如果是在屏幕左侧的1/5，调亮度
+                        if (startX < width /5){
+                            adjustBrightness(percentage);
+                            return true;
+                        }
+                        // 如果是在屏幕右侧的1/5，调音量
+                        else if (startX > width *4/5){
+                            adjustVolume(percentage);
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+        // 对View进行touch监听,但我们自己不去判断各种touch动作了（我们用系统提供的手势处理）
+        adjustView.setOnTouchListener(new OnTouchListener() {
+            // 按下事件时(也代码着马上将开始手势处理)获取到当前音量及亮度
+            // 使用ACTION_MASK是为了过滤掉多点触屏事件
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
 //                gesturedetector.onTouchEvent(motionEvent);
-//                return true;
-//            }
-//        });
+                if ((motionEvent.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN){
+                    currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    currentBrightness = window.getAttributes().screenBrightness;
+                }
+                gesturedetector.onTouchEvent(motionEvent);
+                CustomMediaController.this.show();
+                return true;
+            }
+        });
+    }
+
+    private void adjustVolume(float percentage) {
+        // 计算出目标音量
+        int targeVolume = (int) ((percentage * maxVolume) + currentVolume);
+        targeVolume = targeVolume > maxVolume ?maxVolume :targeVolume;
+        targeVolume = targeVolume < 0 ? 0:targeVolume;
+        // 设置音量
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,targeVolume,AudioManager.FLAG_SHOW_UI);
+    }
+
+    private void adjustBrightness(float percentage) {
+        // 计算出目标亮度
+        float targetBrightness = percentage + currentBrightness;
+        targetBrightness = targetBrightness > 1.0f ? 1.0f :targetBrightness;
+        targetBrightness = targetBrightness < 0.01f ? 0.01f :targetBrightness;
+        // 设置亮度
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.screenBrightness = targetBrightness;
+        window.setAttributes(layoutParams);
     }
 }
